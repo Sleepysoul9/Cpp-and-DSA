b04a651dc3147f60167ec9aed96023a2074d4465
Sleepysoul9 <kushagrastudy.9@gmail.com>
2025-12-22 18:21:59 +0530

    started learning functions

---
+++ functions/func_learn.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printname(string name){
+    cout << "hello " << name << endl;
+}
+
+int main() {
+    string name ;
+    cout << "enter name ";
+    cin >> name ;
+
+    printname(name);
+    
+    return 0;
+}
